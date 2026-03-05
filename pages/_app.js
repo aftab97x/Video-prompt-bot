a0f876c58fa189bcd3b30f1d45c98f384c86fa06
aftab97x <aftabul2427@gmail.com>
2026-03-05 15:25:04 +0600

    Update meta tags in _app.js for charset and color scheme

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,12 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta name="color-scheme" content="dark light" />
+        <meta charSet="UTF-8" />
+        <meta name="color-scheme" content="dark" />
         <script src="https://telegram.org/js/telegram-web-app.js" />
         <title>Video Prompt Generator</title>
       </Head>
       <Component {...pageProps} />
     </>
   );
-    }
+}
